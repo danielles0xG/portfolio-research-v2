32087808bda055e04aa3aa508cce984d610314c6
Daniel G <danielifguerrero@gmail.com>
2023-05-11 13:40:38 -0400

    update title

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,6 +1,6 @@
 export const getGlobalData = () => {
   const name = "Daniel G"
-  const blogTitle = "Blockchain Development Portfolio & Security Research"
+  const blogTitle = "Blockchain Dev Portfolio & Security Research"
   const footerText = "Web3 Portfolio All rights reserved"
   return {
     name,
